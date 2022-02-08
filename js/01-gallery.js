74d4d919183df8c2a7f69f51de665e713e0ef4eb
Alexander Pikurin <pikchas95@gmail.com>
2022-02-08 20:41:12 +0200

    01-gallery_(make)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const makeGallery = galleryItems.map(item => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}" onclick="event.preventDefault()">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+}).join('');
+
+gallery.insertAdjacentHTML('beforeend', makeGallery);
+
+console.log(makeGallery);
