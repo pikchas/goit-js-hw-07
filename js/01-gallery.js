d4627c31d3e2eb52c070314abb19fd894f68138f
Alexander Pikurin <pikchas95@gmail.com>
2022-02-09 18:09:53 +0200

    task-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,7 @@ const gallery = document.querySelector('.gallery');
 
 const makeGallery = galleryItems.map(item => {
     return `<div class="gallery__item">
-  <a class="gallery__link" href="${item.original}" onclick="event.preventDefault()">
+  <a class="gallery__link" href="${item.original}" >
     <img
       class="gallery__image"
       src="${item.preview}"
@@ -16,5 +16,48 @@ const makeGallery = galleryItems.map(item => {
 }).join('');
 
 gallery.insertAdjacentHTML('beforeend', makeGallery);
+gallery.addEventListener('click', onClick);
+
+
+
+function onClick(e) {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  e.preventDefault();
+  modalShow(e.target.dataset.source);
+}
+
+let instance;
+function modalShow(src) {
+  instance = basicLightbox.create(
+    `<div class="modal">
+      <img src="${src}" style="height:95vh; display:block"></img>
+    </div>`,
+    {
+      onShow: instance => {
+        addListener();
+      },
+      onClose: instance => {
+        removeListener();
+      },
+    },
+  );
+  instance.show();
+}
+
+function addListener() {
+  window.addEventListener('keydown', onEscClick);
+}
+
+function onEscClick(e) {
+  if (e.code === 'Escape') {
+    instance.close();
+  }
+}
+
+function removeListener() {
+  window.removeEventListener('keydown', onEscClick);
+}
 
 console.log(makeGallery);
