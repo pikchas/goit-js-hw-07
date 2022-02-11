ecc747ded0ab17b1618a72e111eae958e2bab132
Alexander Pikurin <pikchas95@gmail.com>
2022-02-11 17:33:57 +0200

    simleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryListMarkup = document.querySelector('.gallery');
+const galleryImages = makeGallery(galleryItems)
+galleryListMarkup.insertAdjacentHTML('beforeend', galleryImages);
 
-console.log(galleryItems);
+function makeGallery(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <a class="gallery__item" href="${original}">
+          <img class=" gallery__image" src="${preview}" alt="${description}">
+        </a>
+      `
+    )
+.join('');
+}
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
+
